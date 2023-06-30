f1ab75149c95092ca3a453dc6a297926bb9163c4
matgomiero <matgomiero@gmail.com>
2023-06-30 16:32:43 +0200

    Added Skull Model

--- Im_gine/Im_gine.cpp
+++ Im_gine/Im_gine.cpp
@@ -942,6 +942,10 @@ private:
         generateMipmaps(textureImage, VK_FORMAT_R8G8B8A8_SRGB, texWidth, texHeight, mipLevels);
     }
 
+
+
+
+
     void generateMipmaps(VkImage image, VkFormat imageFormat, int32_t texWidth, int32_t texHeight, uint32_t mipLevels) {
         // Check if image format supports linear blitting
         VkFormatProperties formatProperties;
@@ -1205,11 +1209,74 @@ private:
 
     void loadAssets()
     {
-        Model model;
-        loadAssetAssimp(MODEL_PATH, &model);
 
-        scene.models.push_back(std::move(model));
+        std::vector<std::string> modelPaths= {
+            "models/viking_room.obj"
+        };
+
+        std::vector<std::string> texturePaths = {
+            "textures/viking_room.png"
+        };
+
+
+        for (size_t i = 0; i < modelPaths.size(); i++)
+        {
+            Model model;
+            loadAssetAssimp(modelPaths[i], &model);
+            scene.models.push_back(std::move(model));
+        }
+
+
+        //for (size_t i = 0; i < modelPaths.size(); i++)
+        //{
+        //    Model model;
+        //    loadTexture(modelPaths[i], &model);
+        //    scene.models.push_back(std::move(model));
+        //}
+
+
+        /*Model model;
+        loadAssetAssimp(MODEL_PATH, &model);
+        scene.models.push_back(std::move(model));*/
     }
+
+
+
+    void loadTexture(const std::string& path)
+    {
+        int texWidth, texHeight, texChannels;
+        stbi_uc* pixels = stbi_load(path.c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+        VkDeviceSize imageSize = texWidth * texHeight * 4;
+        mipLevels = static_cast<uint32_t>(std::floor(std::log2(std::max(texWidth, texHeight)))) + 1;
+
+        if (!pixels) {
+            throw std::runtime_error("failed to load texture image!");
+        }
+
+        VkBuffer stagingBuffer;
+        VkDeviceMemory stagingBufferMemory;
+        createBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+        void* data;
+        vkMapMemory(device, stagingBufferMemory, 0, imageSize, 0, &data);
+        memcpy(data, pixels, static_cast<size_t>(imageSize));
+        vkUnmapMemory(device, stagingBufferMemory);
+
+        stbi_image_free(pixels);
+
+        createImage(texWidth, texHeight, mipLevels, VK_SAMPLE_COUNT_1_BIT, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, textureImage, textureImageMemory);
+
+        transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, mipLevels);
+        copyBufferToImage(stagingBuffer, textureImage, static_cast<uint32_t>(texWidth), static_cast<uint32_t>(texHeight));
+        //transitioned to VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL while generating mipmaps
+
+        vkDestroyBuffer(device, stagingBuffer, nullptr);
+        vkFreeMemory(device, stagingBufferMemory, nullptr);
+
+        generateMipmaps(textureImage, VK_FORMAT_R8G8B8A8_SRGB, texWidth, texHeight, mipLevels);
+    }
+
+
 
     bool loadAssetAssimp(std::string path,Model* model)
     {
